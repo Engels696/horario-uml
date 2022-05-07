9551b0f8350050fba934bef5be56ad3796247a83
Engels696 <est.ediazu@uml.edu.ni>
2022-05-07 12:59:40 -0600

    actualizacion de proyecto

---
+++ app/Http/Livewire/UniversidadComponent.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\Universidad;
+class UniversidadComponent extends Component
+{
+    public function render()
+    {
+     
+
+        return view('livewire.universidad-component');
+        
+    }
+}
